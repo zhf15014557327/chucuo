6b8e830016fbf4ea575bf4433093ca8159c81b77
Your Name <118735992@.con>
2019-03-19 22:17:38 +0800

    出错求解

---
+++ src/components/jishuqi.vue
@@ -0,0 +1,55 @@
+<template>
+     <div class="conten-jishu">
+      <h3>我的计数器</h3>
+      <input type="button" value="+" @click="jia" >
+      <span>{{num}}</span>
+      <input type="button" value="-" @click="jian">
+    </div>
+</template>
+<script>
+export default {
+      name: "jishu",
+     data(){
+    return{
+      num:10,
+      dfumax:35,
+      dfumin:0,
+      dfbushu:1
+    }
+  },
+  
+  // 方法
+  methods:{
+      jia(){
+          this.num +=this.dfbushu
+          if(this.num>this.dfumax){
+              this.num=this.dfumax
+          };
+       
+      },
+      jian(){
+            this.num-=this.dfbushu
+            if(this.num<this.dfumin){
+                this.num=this.dfumin
+            };
+        
+      }
+  },
+}
+</script>
+<style lang="scss">
+.conten-jishu{
+  text-align: center;
+  margin: 300px auto;
+  width: 150px;
+  input{
+    width: 50px;
+    height: 100%;
+  }
+  span{
+    display: inline-block;
+    width: 50px;
+    height: 100%;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-
+// 导入vue-router路由
+import VueRouter from 'vue-router'
+// 因为是vue的插件所以要use
+Vue.use(VueRouter)
+// 导入组件
+import jishuqi from "./components/jishuqi.vue"
 new Vue({
+// 挂载实例
+   router:new VueRouter({
+    routes:[
+      // {
+      //   path:'./',
+      //   component:jishuqi
+      // },
+      {
+        path:'./jishuqi',
+        component:jishuqi
+      },
+    ]
+  }),
   render: h => h(App),
 }).$mount('#app')
